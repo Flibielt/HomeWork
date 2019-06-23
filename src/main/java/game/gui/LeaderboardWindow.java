f074bd667b50bef8c360f99cc89e78db56f02e72
Csaba Hermann <hcsabi98@gmail.com>
2019-06-23 22:11:22 +0200

    Bug fix in leader board window

--- src/main/java/game/gui/LeaderboardWindow.java
+++ src/main/java/game/gui/LeaderboardWindow.java
@@ -38,20 +38,29 @@ public class LeaderboardWindow {
      * @param vBox the {@code VBox} is the root of the current {@code Scene}
      */
     private static void getLeaderboard(Game game, VBox vBox) {
-        HBox hBox = new HBox();
-        hBox.setAlignment(Pos.CENTER);
-        hBox.setSpacing(5);
+
         for (int i = 0; i < game.getLeaderboardSize(); i++) {
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER);
+            hBox.setSpacing(5);
+
             Text position = new Text(i + 1 + ". ");
             Text playerName = new Text(game.getPlayerFromLeaderboard(i).getName());
             Text playerScore = new Text(Integer.toString(game.getPlayerFromLeaderboard(i).getSteps()));
             hBox.getChildren().addAll(position, playerName, playerScore);
+
+            vBox.getChildren().add(hBox);
         }
 
         if (game.getLeaderboardSize() == 0) {
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER);
+            hBox.setSpacing(5);
+
             Text emptyLeaderboard = new Text("The leaderboard is empty");
             hBox.getChildren().add(emptyLeaderboard);
+
+            vBox.getChildren().add(hBox);
         }
-        vBox.getChildren().add(hBox);
     }
 }
